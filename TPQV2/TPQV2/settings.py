6504aaf045f0cf173696dad1d2a884d2208d78e0
Suyash <suyashgokhale55@gmail.com>
2020-07-26 15:02:54 -0500

    first

--- TPQV2/TPQV2/settings.py
+++ TPQV2/TPQV2/settings.py
@@ -14,6 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +30,7 @@ SECRET_KEY = 'xe*492*#hczaewu2qiz!kfu2^hu-3^w&p+0%5w56h7rb7th=#@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
